d8cc54f21371fd00472b818e54151b15474c5d66
jismi123 <73015077+jismi123@users.noreply.github.com>
2022-11-19 10:37:51 +0530

    Create indexed.c

---
+++ exp1/fileallocation/indexed.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int f[50],index[50],i,n,st,len,j,c,k,ind,count=0;
+	for(i=0;i<50;i++)
+		f[i]=0;
+	x:printf("Enter the index block:");
+	scanf("%d",&ind);
+	if(f[ind]!=1)
+	{
+		printf("Enter the no.of blocks needed & no. of files for the index %d on the disk:\n",ind);
+		scanf("%d",&n);
+	}
+	else
+	{
+		printf("%d index is already allocated\n",ind);
+		goto x;
+	}
+	y:count=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&index[i]);
+		if(f[index[i]]==0)
+		
+			count++;
+	}
+	if(count==n)
+	{
+		for(j=0;j<n;j++)
+			f[index[j]]=1;
+		printf("Allocated!!!\n");
+		printf("File Indexed\n");
+		for(k=0;k<n;k++)
+			printf("%d---->%d:%d\n",ind,index[k],f[index[k]]);
+	}
+	else
+	{
+		printf("File in the index is alraedy allocated\n");
+		printf("Enter another file indexed:");
+		goto y;
+	}
+	printf("DO you want to enter more files(Yes-1/No-0):");
+	scanf("%d",&c);
+	if(c==1)
+		goto x;
+	else 
+		exit(0);
+}
+
+
+/*
+
+---------OUTPUT-----------
+
+Enter the index block:1
+Enter the no.of blocks needed & no. of files for the index 1 on the disk:
+4
+3 2 1 5
+Allocated!!!
+File Indexed
+1---->3:1
+1---->2:1
+1---->1:1
+1---->5:1
+DO you want to enter more files(Yes-1/No-0):0
+
+*/
